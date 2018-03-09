a4ea45af430e5d95a7c4074a278a227b3e99c2ed
Pere Pages <pere.pages@weareblueorange.com>
2018-03-09 14:14:40 +0100

    feat: toggle buttons

--- src/app/main/buttons-indicators.component.ts
+++ src/app/main/buttons-indicators.component.ts
@@ -94,7 +94,26 @@ import { Component, OnInit } from '@angular/core';
 
     <h3>Button Toggle</h3>
 
+    <h4>Single Selection</h4>
+
     <mat-button-toggle-group #group="matButtonToggleGroup">
+      <mat-button-toggle value="left">
+        <mat-icon>format_align_left</mat-icon>
+      </mat-button-toggle>
+      <mat-button-toggle value="center">
+        <mat-icon>format_align_center</mat-icon>
+      </mat-button-toggle>
+      <mat-button-toggle value="right">
+        <mat-icon>format_align_right</mat-icon>
+      </mat-button-toggle>
+      <mat-button-toggle value="justify" disabled>
+        <mat-icon>format_align_justify</mat-icon>
+      </mat-button-toggle>
+    </mat-button-toggle-group>
+
+    <h4>Multiple Selection</h4>
+
+    <mat-button-toggle-group multiple>
       <mat-button-toggle value="left">
         <mat-icon>format_align_left</mat-icon>
       </mat-button-toggle>
