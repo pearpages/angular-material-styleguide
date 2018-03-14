6fdfcb32cb197c69a8e503f325d39f7306828f9f
pearpages <pere@soms.cat>
2018-03-14 20:11:40 +0100

    feat: chips and icons

--- src/app/main/sections/buttons-indicators.component.ts
+++ src/app/main/sections/buttons-indicators.component.ts
@@ -119,9 +119,17 @@ import { Component, OnInit } from '@angular/core';
     </sty-expansion>
 
     <sty-expansion [title]="'Chips'">
+      <p>If you want to use colors the chip must be selected=true</p>
+      <mat-chip-list>
+        <mat-chip>One fish</mat-chip>
+        <mat-chip>Two fish</mat-chip>
+        <mat-chip color="primary" selected="true">Primary fish</mat-chip>
+        <mat-chip color="accent" selected="true">Accent fish</mat-chip>
+      </mat-chip-list>
     </sty-expansion>
 
     <sty-expansion [title]="'Icon'">
+      <mat-icon>home</mat-icon>
     </sty-expansion>
 
     <sty-expansion [title]="'Progress spinner'">
