089f362b64f83ddc61f9b81b5ce2fa84c30fe609
pearpages <pere@soms.cat>
2018-03-14 20:27:35 +0100

    feat: buttons section

--- src/app/main/sections/buttons-indicators.component.ts
+++ src/app/main/sections/buttons-indicators.component.ts
@@ -142,6 +142,34 @@ import { Component, OnInit } from '@angular/core';
     </sty-expansion>
 
     <sty-expansion [title]="'Progress bar'">
+      <ul>
+        <li>Determinate</li>
+        <li>Indeterminate</li>
+        <li>Buffer</li>
+        <li>Query</li>
+      </ul>
+      <mat-list>
+        <mat-list-item>
+          <h4 matLine>Determinate</h4>
+          <mat-progress-bar matLine mode="determinate" value="40"></mat-progress-bar>
+          <mat-divider></mat-divider>
+        </mat-list-item>
+        <mat-list-item>
+          <h4 matLine>Indeterminate</h4>
+          <mat-progress-bar matLine mode="indeterminate"></mat-progress-bar>
+          <mat-divider></mat-divider>
+        </mat-list-item>
+        <mat-list-item>
+          <h4 matLine>Buffer</h4>
+          <mat-progress-bar matLine mode="buffer"></mat-progress-bar>
+          <mat-divider></mat-divider>
+        </mat-list-item>
+        <mat-list-item>
+          <h4 matLine>Query</h4>
+          <mat-progress-bar matLine mode="query"></mat-progress-bar>
+        </mat-list-item>
+      </mat-list>
+
     </sty-expansion>
 
   </sty-page>
