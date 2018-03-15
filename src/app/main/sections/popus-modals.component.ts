2b58555e1cd925226764c33dce2923d9e92f2b57
Pere Pages <pere.pages@weareblueorange.com>
2018-03-15 09:12:29 +0100

    feat: dialog example

--- src/app/main/sections/popus-modals.component.ts
+++ src/app/main/sections/popus-modals.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'sty-popups-modals',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   <sty-page title="Layout">
 
     <sty-expansion [title]="'Dialog'">
-
+      <p>In this example look at entryComponents and how uses @Inject and gets the dialog reference</p>
+      <p><a href="https://material.angular.io/components/dialog/overview">Dialog docs</a></p>
+      <p><button mat-raised-button (click)="openDialog()">Pick one</button></p>
     </sty-expansion>
 
     <sty-expansion [title]="'Snackbar'">
@@ -22,5 +25,46 @@ import { Component } from '@angular/core';
 })
 
 export class PopupsModalsComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { name: 'John', animal: 'Tiger' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      alert('The dialog was closed');
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  template: `
+  <h1 mat-dialog-title>Hi {{data.name}}</h1>
+  <div mat-dialog-content>
+    <p>What's your favorite animal?</p>
+    <mat-form-field>
+      <input matInput [(ngModel)]="data.animal">
+    </mat-form-field>
+  </div>
+  <div mat-dialog-actions>
+    <button mat-button (click)="onNoClick()">No Thanks</button>
+    <button mat-button [mat-dialog-close]="data.animal" cdkFocusInitial>Ok</button>
+  </div>
+  `,
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
--- src/app/main/sections/sections.module.ts
+++ src/app/main/sections/sections.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from './../../shared/shared.module';
 import { FormsControlComponent } from './form-controls.component';
 import { LayoutComponent } from './layout.component';
-import { PopupsModalsComponent } from './popus-modals.component';
+import { PopupsModalsComponent, DialogOverviewExampleDialog } from './popus-modals.component';
 import { ButtonsIndicatorsComponent } from './buttons-indicators.component';
 
-const components = [ButtonsIndicatorsComponent, FormsControlComponent, LayoutComponent, PopupsModalsComponent];
+const components = [ButtonsIndicatorsComponent, FormsControlComponent, LayoutComponent, PopupsModalsComponent, DialogOverviewExampleDialog];
 
 const routes: Routes = [
   { path: 'examples/buttons-and-indicators', component: ButtonsIndicatorsComponent },
@@ -17,9 +18,10 @@ const routes: Routes = [
   { path: 'examples/layout', component: LayoutComponent },
   { path: 'examples/popups-modals', component: PopupsModalsComponent },
 ];
-
+// TODO: create dialog example own module and move the entryComponents
 @NgModule({
-  imports: [ReactiveFormsModule, CommonModule, SharedModule, RouterModule.forRoot(routes)],
+  entryComponents: [DialogOverviewExampleDialog],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, SharedModule, RouterModule.forRoot(routes)],
   exports: [RouterModule, ...components],
   declarations: components,
 })
