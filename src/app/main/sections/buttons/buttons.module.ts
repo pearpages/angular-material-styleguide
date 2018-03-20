f08b6a454847a4943992899ab9b2fb1ea2c1e74b
Pere Pages <pere.pages@weareblueorange.com>
2018-03-20 16:11:18 +0100

    chore: buttons module

---
+++ src/app/main/sections/buttons/buttons.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ButtonsIndicatorsComponent } from './buttons-indicators.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [ButtonsIndicatorsComponent],
+  providers: [],
+})
+export class ButtonsModule { }
